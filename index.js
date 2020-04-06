973e565d3f4f6624fdc0ddb3777f60c92e7f7187
Rendy Febry <rendyfebry@gmail.com>
2020-04-06 18:24:09 +0700

    Gardening

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Numbering = require("./lib/numbering");
 
 const num = new Numbering();
 
-console.log("Sum", num.Sum(3, 4));
-console.log("Multiply", num.Multiply(3, 4));
-console.log("GetPrimes", num.GetPrimes(4));
-console.log("GetFibonaccy", num.GetFibonaccy(4));
+console.log("Sum(3,4): \t\t", num.Sum(3, 4));
+console.log("Multiply(3,4): \t\t", num.Multiply(3, 4));
+console.log("GetPrimes(4): \t\t", num.GetPrimes(4));
+console.log("GetFibonaccy(4): \t", num.GetFibonaccy(4));
