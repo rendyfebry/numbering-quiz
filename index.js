dcd57701200e40d887b2c1efbc1552e1d2cad33a
Rendy Febry <rendyfebry@gmail.com>
2020-04-06 16:45:35 +0700

    Implement function

---
+++ index.js
@@ -0,0 +1,6 @@
+const { Sum, Multiply, GetPrimes, GetFibonaccy } = require("./lib/numbering");
+
+console.log("Sum", Sum(3, 4));
+console.log("Multiply", Multiply(3, 4));
+console.log("GetPrimes", GetPrimes(4));
+console.log("GetFibonaccy", GetFibonaccy(4));
